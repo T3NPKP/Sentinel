6fee09aeb8ed01c28e7dd30f4dc77da78b0686a1
Kepei Lei <klei@wpi.edu>
2019-05-22 10:31:00 +0800

    Download done working on plotting

--- Downloader.py
+++ Downloader.py
@@ -8,23 +8,26 @@ class Downloader:
         self.api = SentinelAPI(str_username, str_password, str_link)
         self.products = None
 
-    def search_polygon(self, footprint: object, str_date_start: object, str_date_end: object, str_platform_name: object, percentage: object) -> object:
+    def search_polygon(self, footprint: object, str_date_start: str,
+                       str_date_end: str, str_platform_name: str, percentage: object):
         print('searching')
         self.products = self.api.query(footprint,
                                        date=(str_date_start, str_date_end),
                                        platformname=str_platform_name,
                                        cloudcoverpercentage=(percentage[0], percentage[1]))
         size = self.api.get_products_size(self.products)
-        print(f'found {size} of data')
+        print(f'found {size}GiB of data')
 
-    def download_products(self, path):
-        self.api.download_all(self.products, directory_path=path, max_attempts=max_attempt, checksum=True)
+    def download_products(self, path, download_file):
+        if download_file:
+            self.api.download_all(self.products, directory_path=path, max_attempts=max_attempt, checksum=True)
         print('downloaded')
         df_products = self.api.to_dataframe(self.products)
         return df_products
 
-    def download_geoproduct(self, path):
-        self.api.download_all(self.products, directory_path=path, max_attempts=max_attempt, checksum=True)
+    def download_geoproduct(self, path, download_file):
+        if download_file:
+            self.api.download_all(self.products, directory_path=path, max_attempts=max_attempt, checksum=True)
         print('download Geos')
         gdf_products = self.api.to_geodataframe(self.products)
         return gdf_products
--- Test.py
+++ Test.py
@@ -1,13 +1,18 @@
 from sentinelsat import read_geojson, geojson_to_wkt
 from Downloader import Downloader
+import geopandas
 
 username = 'kepeilei'
 password = '297089702yuxiaO'
 path = '/Users/DavidLei/PycharmProjects/untitled/testfile.geojson'
 link: str = 'https://scihub.copernicus.eu/apihub/'
+from_satelite: str = 'Sentinel-2'
 
 downloader = Downloader(username, password, link)
 wkt = geojson_to_wkt(read_geojson(path))
-downloader.search_polygon(wkt, '20190501', '20190503', str_platform_name='Sentinel-2', percentage=(0, 100))
-df_products = downloader.download_products('/Users/DavidLei/PycharmProjects/untitled')
-gdf_products = downloader.download_geoproduct('/Users/DavidLei/PycharmProjects/untitled')
+downloader.search_polygon(wkt, '20190502', '20190503', str_platform_name=from_satelite, percentage=(0, 100))
+#df_products = downloader.download_products('/Users/DavidLei/PycharmProjects/untitled', False)
+gdf_products = downloader.download_geoproduct('/Users/DavidLei/PycharmProjects/untitled', False)
+print(gdf_products.count())
+gdf_products.head()
+gdf_products.plot()
